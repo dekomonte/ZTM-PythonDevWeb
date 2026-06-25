ea1d74bc77c64279742da15bece0dddd55edff84
Andressa Monteiro <dekomonte@yahoo.com.br>
2026-06-25 18:57:09 -0300

    Update app.py - Links dinâmicos

--- app.py
+++ app.py
@@ -6,15 +6,6 @@ app = Flask(__name__)
 def my_home():
     return render_template("index.html")
 
-@app.route("/works")
-def works():
-    return render_template("works.html")
-
-@app.route("/about")
-def about():
-    return render_template("about.html")
-
-@app.route("/contact")
-def contact():
-    return render_template("contact.html")
-
+@app.route("/<string:page_name>")
+def html_page(page_name):
+    return render_template(page_name)
